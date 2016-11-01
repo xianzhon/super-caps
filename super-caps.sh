43748b2779a5a4fa1ea184070f6aa9b6796de13c
Wang Xianzhong <wang_xi@worksap.co.jp>
2016-11-01 16:31:00 +0800

    Use Alt_R as F12 and Alt in the same time

--- super-caps.sh
+++ super-caps.sh
@@ -2,7 +2,7 @@
 
 xmodmap xmodmaprc
 xcape -e 'Control_L=Escape'
-xcape -t 300 -e 'Control_R=Caps_Lock'
+# xcape -t 300 -e 'Control_R=Caps_Lock'
 xcape -t 300 -e 'Shift_R=Home'
 # xcape -t 300 -e 'Alt_L=F12'
-# xcape -t 300 -e 'Alt_R=F12'
+xcape -t 500 -e 'Alt_R=F12'
